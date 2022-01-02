1c538ca5fe4810f851f0d5293d8918b4a00c77f7
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-01-02 16:31:12 +0100

    SearchBar Done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,11 @@ import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
 const business = {
-  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
   name: "MarginOtto Pizzeria",
   address: "1010 Paddington Way",
-  city: "Flavortown",
+  city: "Bordertown",
   state: "NY",
   zipCode: "10101",
   category: "Italian",
@@ -16,14 +17,18 @@ const business = {
   reviewCount: 90,
 };
 
-let businesses = [business, business, business, business, business, business];
+const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
     );
